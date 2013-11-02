7feb67b3adb0898c280f520d17afdf7326923d5a
Fernando Pereira <fernandhenriqp@gmail.com>
2013-11-02 11:49:21 -0200

    blop

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ typedef enum e{ CPY=0, REP=1, DEL=2, INS=3, TWD=4, KLL=5} EOp;
 typedef struct {
   int cost;
   int i, j;
+  TCell* prev;
+  int end;
 } TCell;
 
 int main(void){
@@ -20,12 +22,14 @@ int main(void){
   int costs[6] = {3, 5, 2, 3, 2, 1};  // só teste
 
   int k,l,q;
-  TCell m[6][CSIZE];
-  TCell aux[6];
+  TCell m[5][CSIZE];
+  TCell aux[5];
   int cost;
   int n = strlen(y);
   int i, j = 0;  // i => indice de x; j => indice de y
   
+
+  // Inicializacao
   m[0][CPY].i = m[0][CPY].j = 1;
   m[0][CPY].cost = INF;
   if( x[i] == y[j] ){
@@ -47,62 +51,88 @@ int main(void){
   m[0][TWD].cost = INF;
   if( x[0] == y[1] && x[1] == y[0] ){
     m[0][TWD].cost = costs[TWD];
-  }    
+  }
+  
+  m[0][CPY].prev = m[0][REP].prev = m[0][DEL].prev = m[0][INS].prev = m[0][TWD].prev = NULL; 
+  
     
-    
-  for( k = 1; k < n; k++ ){
-    
-    // Preenche aux;
-    for( q = 0; q < 6; q++){
-      op( q, x, y, m[q][k-1].i, m[q][k-1].j, &aux, costs );
-    }
+  for( k = 1; k < n; k++ ){ // Coluna (passo)
+    for( l = 0; l < 5; l++){ // operacao
+      
+      // Preenche aux;
+      for( q = 0; q < 6; q++){  
+	op( l, x, y, &m[q][k-1], aux, costs );   // O que varia: OP anterior
+      }
 
-    for( l = 0; l < 6; l++){
-
-      // analisar com o custo da proxima operacao
-
-
+      // min aux
+      m[l][k] = min(aux);
     }
   }
+  
+  /// QUANDO TERMINAR?
+
+
 }
 
 
-void op(int p, char* x, char* y, int i, int j, TCell* ct, int* costs){
-  switch(p){
+TCell min_cell(TCell ct[]){
+  TCell min;
+  int i;
+
+  min = ct[0]; 
+  for(i=1; i<6; i++){
+    if( ct[i].cost < min.cost || (ct[i].cost == min.cost && ct[i].i > min.i)){
+      min = ct[i];
+    }
+  }
+  
+  return min;
+}
+
+
+
+void op(int p, int pop, char* x, char* y, TCell* o, TCell* ct, int* costs){
+  //pop  -> previous operation: posicao de ct; (ligado a "o")
+
+  ct[pop].prev = o;
+  switch(p){  // Decisao sobre a operacao a ser executada agora
   case CPY:
-    ct[CPY].i = i+1;
-    ct[CPY].j = j+1;
-    ct[CPY].cost = INF;
-    if( x[i] == y[j] ){
-      ct[CPY].cost = costs[CPY];      
+    ct[pop].i = o->i+1;
+    ct[pop].j = o->j+1;
+    ct[pop].cost = INF;
+    if( x[o->i] == y[o->j] ){
+      ct[pop].cost = costs[CPY] + o->cost;      
     }
     break;
 
   case REP:
-    ct[REP].i = i+1;
-    ct[REP].j = j+1;
-    ct[REP] = costs[REP];
+    ct[pop].i = o->i+1;
+    ct[pop].j = o->j+1;
+    ct[pop] = costs[REP] + o->cost;
     break;
 
   case DEL:
-    ct[DEL].i = i+1;
-    ct[DEL] = costs[DEL];
+    ct[pop].i = o->i+1;
+    ct[pop] = costs[DEL] + o->cost;
     break;
 
   case INS:
-    ct[INS].j = j+1;
-    ct[INS] = costs[INS];
+    ct[pop].j = o->j+1;
+    ct[pop] = costs[INS] + o->cost;
     break;
 
   case TWD:
-    ct[TWD].i += 2;
-    ct[TWD].j += 2;
-    ct[TWD].cost = INF;
-    if( x[i] == y[i+1] && x[i+1] == y[i] ){   //// p?!?!?!?!?!
-      ct[TWD].cost = costs[TWD];
+    ct[pop].i += 2;
+    ct[pop].j += 2;
+    ct[pop].cost = INF;
+    if( x[o->i] == y[o->i+1] && x[o->i+1] == y[o->i] ){
+      ct[pop].cost = costs[TWD] + o->cost;
     }
     break; 
   }    
+
+  
+
 } 
 
 
